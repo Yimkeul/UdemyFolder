dca91c4fca03d18cff0dbb06be48fa9a541a03d6
Yimkeul <leesjun29@gmail.com>
2023-07-23 17:09:53 +0900

    feat : Card Component

---
+++ Hike/Hike/Component/CardView.swift
@@ -0,0 +1,31 @@
+//
+//  CardView.swift
+//  Hike
+//
+//  Created by yimkeul on 2023/07/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        ZStack{
+            CustomBackgroundView()
+            ZStack{
+                Circle()
+                    .fill(LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .frame(width: 256, height: 256)
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+        .frame(width: 320, height: 570)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
