98cd106d91001ab3afa4426fde57cc0acd6bf063
Yimkeul <leesjun29@gmail.com>
2023-07-27 23:59:16 +0900

    feat : card header view

--- Hike/Hike/Component/CardView.swift
+++ Hike/Hike/Component/CardView.swift
@@ -11,13 +11,38 @@ struct CardView: View {
     var body: some View {
         ZStack{
             CustomBackgroundView()
-            ZStack{
-                Circle()
-                    .fill(LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .frame(width: 256, height: 256)
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+            VStack{
+                // MARK: HEADER
+                VStack(alignment: .leading){
+                    HStack{
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(LinearGradient(colors: [.customGrayLight , .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                        Spacer()
+                        Button {
+                            print("Button pressed")
+                        } label: {
+                            CustomButtonView()
+                        }
+
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and familes.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
+                // MARK: CONTENT
+                ZStack{
+                    Circle()
+                        .fill(LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                // MARK: FOOTER
             }
         }
         .frame(width: 320, height: 570)
