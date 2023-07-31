ff45003878ab1cacedda2b7e0958f223865e2a70
Yimkeul <leesjun29@gmail.com>
2023-07-31 15:10:56 +0900

    feat : restart app

--- Restart/Restart/Screens/ContentView.swift
+++ Restart/Restart/Screens/ContentView.swift
@@ -8,22 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @AppStorage("onboarding") var isOnBoardingViewActive: Bool = true
-
-    var body: some View {
-        ZStack{
-            if isOnBoardingViewActive {
-                OnBoardingView()
-            } else {
-                HomeView()
-            }
-        }
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+  
+  var body: some View {
+    ZStack {
+      if isOnboardingViewActive {
+        OnboardingView()
+      } else {
+        HomeView()
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
